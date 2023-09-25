dc5aafa5e3140a3917db5c15d56deda007f9002b
branjosef <branjosef@gmail.com>
2023-09-25 18:12:44 -0500

    asdsdsa
    adsasdsa

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,20 +52,6 @@ export const Home = () => {
 
         <div className={styles.grid}>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
             target="_blank"
